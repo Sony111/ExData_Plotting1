35c47e0798f161cef88ce8b2eaa02540c19b3272
Sony111 <Sony111@users.noreply.github.com>
2014-10-08 12:47:54 -0700

    Update plot1.R

--- figure/plot1.R
+++ figure/plot1.R
@@ -6,11 +6,11 @@ nrow(PowerConsumption)
 object.size(PowerConsumption) # 91853280 bytes
 
 # subsetting February 1 st and 2 nd days of 2007 Year
-PCDate<-subset(PowerConsumption,PowerConsumption$Date == "1/2/2007" | PowerConsumption$Date == "2/2/2007",select=Date:Sub_metering_3) 
+PCDate<-subset(PowerConsumption,PowerConsumption$Date == "2007-2-1" | PowerConsumption$Date == "2007-2-2",select=Date:Sub_metering_3) 
 
 object.size(PCDate) #541848 bytes
 
-PCDate$Date <- strptime(paste(PCDate$Date,PCDate$Time), "%d/%m/%Y %H:%M:%S") # Convereting into Date 
+PCDate$Date <- strptime(paste(PCDate$Date,PCDate$Time), "%Y-%m-%d %H:%M:%S") # Convereting into Date 
 head(PCDate$Global_active_power)
 
 hist(as.integer(PCDate$Global_active_power),col="red",main="Global Active Power",xlab="Global Active Power(Kilowatts)",breaks=12,axes=FALSE)
