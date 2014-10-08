1e21175484b5ce0d1d4a430adc8b9a83d7f54117
Sony111 <Sony111@users.noreply.github.com>
2014-10-08 12:51:25 -0700

    Update plot1.R

--- figure/plot1.R
+++ figure/plot1.R
@@ -1,3 +1,4 @@
+# loading the data
 PowerConsumption  <- read.table(file="./exdata-data-household_power_consumption/household_power_consumption.txt",header=TRUE,sep=";")
 PowerConsumption$Date <- as.Date(PowerConsumption$Date, "%d/%m/%Y")
 names(PowerConsumption)
@@ -8,7 +9,7 @@ object.size(PowerConsumption) # 91853280 bytes
 # subsetting February 1 st and 2 nd days of 2007 Year
 PCDate<-subset(PowerConsumption,PowerConsumption$Date == "2007-2-1" | PowerConsumption$Date == "2007-2-2",select=Date:Sub_metering_3) 
 
-object.size(PCDate) #541848 bytes
+object.size(PCDate) #808288 bytes
 
 PCDate$Date <- strptime(paste(PCDate$Date,PCDate$Time), "%Y-%m-%d %H:%M:%S") # Convereting into Date 
 head(PCDate$Global_active_power)
