453ff29eb05c6058290dacd121aa9689e96a9a57
Sony111 <Sony111@users.noreply.github.com>
2014-10-08 22:13:52 -0700

    Update plot4.R

--- figure/plot4.R
+++ figure/plot4.R
@@ -1,9 +1,11 @@
+# Load the data
 data  <- read.table(file="./exdata-data-household_power_consumption/household_power_consumption.txt",header=TRUE,sep=";")
-data$Date <- as.Date(data$Date, "%d/%m/%Y")
+data$Date <- as.Date(data$Date, "%Y-%m-%d")
+# Extract Feb 1 st and 2 nd date data
 part1 <- subset(data, data$Date=="2007-02-01")
 part2 <- subset(data, data$Date=="2007-02-02")
 new_data <- rbind(part1, part2)
-new_data$Date <- as.POSIXct(paste(new_data$Date,new_data$Time), "%d/%m/%Y %H:%M:%S") # changing to POSIX
+new_data$Date <- strptime(paste(new_data$Date,new_data$Time), "%Y-%m-%d %H:%M:%S") # Concatinate date,time into Date
 
 par(mfrow=c(2,2)) #row wise plotting graphs
 par(mar=c(4,4,2,2))
