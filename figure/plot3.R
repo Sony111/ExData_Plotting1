5bf9bac0e4e321a50c907c7e0534058d79d5595f
Sony111 <Sony111@users.noreply.github.com>
2014-10-08 22:06:05 -0700

    Update plot3.R

--- figure/plot3.R
+++ figure/plot3.R
@@ -1,9 +1,11 @@
+# Load the data
 data  <- read.table(file="./exdata-data-household_power_consumption/household_power_consumption.txt",header=TRUE,sep=";")
-data$Date <- as.Date(data$Date, "%d/%m/%Y")
+data$Date <- as.Date(data$Date, "%Y-%m-%d")
+# extract feb 1 st and 2 nd date data
 part1 <- subset(data, data$Date=="2007-02-01")
 part2 <- subset(data, data$Date=="2007-02-02")
 new_data <- rbind(part1, part2)
-new_data$Date <- as.POSIXct(paste(new_data$Date,new_data$Time), "%d/%m/%Y %H:%M:%S") # changing to POSIX
+new_data$Date <- strptime(paste(new_data$Date,new_data$Time), "%Y-%m-%d %H:%M:%S") # Concatenate date and time to Date
 
 plot(new_data$Date, new_data$Sub_metering_1, col = "black", xlab="", ylab="Energy sub metering",type="l")
 lines(new_data$Date, new_data$Sub_metering_2, col = "red")
